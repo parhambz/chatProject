b7726000a177ca8af3f5f3956a905ee25d59c527
parham <bghparham@gmail.com>
2018-06-08 20:27:56 +0430

    good things happend 2

--- headers/userStruct.h
+++ headers/userStruct.h
@@ -4,6 +4,32 @@ struct user{
     char lastname[255];
     char username[255];
     char password[255];
+    int chats [1000];
+    int chatsNumber;
+    char contactLoc[255];
+    char userInfoLoc[255];
+    user(bool flag=false){
+        if (!flag){
+            return;
+        }
+        int lastUserId=getLastUserId();
+        lastUserId++;
+        id=lastUserId;
+        chatsNumber=0;
+        strcpy(userInfoLoc,"../db/users/");
+        char temp[10];
+        sprintf(temp,"%d",id);
+        strcat(userInfoLoc,temp);
+        strcat(userInfoLoc,"/user.bin");
+
+        strcpy(contactLoc,"../db/users/");
+        strcat(contactLoc,temp);
+        strcat(contactLoc,"/contact.bin");
+    }
+    void addChat(int chatId){
+        chats[chatsNumber]=chatId;
+        chatsNumber++;
+    }
     void setInfo(char ifirstname[255],char ilastname[255],char iusername[255],char ipassword[255]){
         strcpy(firstname,ifirstname);
         strcpy(lastname,ilastname);
