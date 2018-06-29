c34c86679cb2317272bf276258fb2858a3c34bbc
parham <bghparham@gmail.com>
2018-06-30 01:56:13 +0430

    almost done

--- client/client.cpp
+++ client/client.cpp
@@ -1,6 +1,9 @@
 #include"../headers/requestStruct.h"
 
-char logedIn[255]="parham";
+char logedIn[255]="";
+int state=0;
+char curChatId[20];
+
 int addUser(char * firstname,char * lastname,char * username,char * password){
     struct request req(logedIn,$"adduser");
     pair firtsnamePair($"firstname",firstname);
@@ -117,8 +120,163 @@ int search (char name[255]){
     response=req.send();
     return 0;
 }
+void getLine(char line[]) {
+    char c = 0;
+    int i = 0;
+    int flag=0;
+    while (c != '\n') {
+        c = getchar();
+        if(c!=' '){
+            flag=1;
+        }
+        if (flag==1){
+        line[i] = c;
+        i++;
+        }
+    }
+    line[i - 1] = 0;
+}
+void getCommand(int start,char line [],char res[]){
+    int len = strlen(line);
+    for (int i=start ;i<len;i++){
+        if(line[i]==' '){
+            res[i-start]='\0';
+            break;
+        }else {
+            res[i-start]=line[i];
+        }
+    }
+}
+void getMsg(char line[],char msg[]){
+    int len = strlen(line);
+    for(int i=8;i<len;i++){
+        msg[i-8]=line[i];
+    }
+
+}
+int menu(){
+    printf("\n\n\n\n");
+    switch(state){
+        case 0:
+            printf("login <username> <password>\n");
+            printf("adduser <firstname> <lastname> <username> <password>\n");
+            break;
+        case 1:
+            printf("newchat <username>\n");
+            printf("newgp <gpname>\n");
+            printf("newchannel <channelname>\n");
+            printf("chatlist\n");
+            printf("logout\n");
+            break;
+        case 2:
+            printf("goto <chatid>\n");
+            printf("main\n");
+            break;
+        case 3:
+            printf ("sendmsg <msg content>");
+            printf("main\n");
+            break;
+    }
+    char line[6000];
+    getLine(line);
+    char command[6000];
+    int start=0;
+    getCommand(start,line,command);
+    if(state==0){
+        if (strcmp(command,"login")==0){
+            start+=strlen(command);
+            char username[255];
+            getCommand(start,line,username);
+            start+=strlen(username);
+            char password[255];
+            getCommand(start,line,password);
+            int res=login(username,password);
+            if (res==1){
+                state=1;
+                menu();
+            }else{
+                menu();
+            }
+        }else if(strcmp(command,"adduser")==0){
+            start+=strlen(command);
+            char firstname[255];
+            getCommand(start,line,firstname);
+            start+=strlen(firstname);
+
+            char lastname[255];
+            getCommand(start,line,lastname);
+            start+=strlen(lastname);
+
+            char username[255];
+            getCommand(start,line,username);
+            start+=strlen(username);
+
+            char password[255];
+            getCommand(start,line,password);
+            int res=addUser(firstname,lastname,username,password);
+            printf("user %d added!",res);
+            menu();
+        }
+    }
+    else if(state=1){
+        if (strcmp(command,"newchat")==0){
+            start+=strlen(command);
+            char username[255];
+            getCommand(start,line,username);
+            int res=newChat(username);
+            printf("chat %d added!",res);
+            menu();
+        }else if (strcmp(command,"newgp")==0){
+            start+=strlen(command);
+            char name[255];
+            getCommand(start,line,name);
+            int res=newGp(name);
+            printf("chat %d added!",res);
+            menu();
+        }else if (strcmp(command,"newchannel")==0){
+            start+=strlen(command);
+            char name[255];
+            getCommand(start,line,name);
+            int res=newchannel(name);
+            printf("chat %d added!",res);
+            menu();
+            }
+        else if (strcmp(command,"logout")==0){
+            printf("logged out!");
+            strcpy(logedIn,"");
+            state=0;
+            menu();
+        }
+
+    }else if(state==2){
+       if (strcmp(command,"goto")==0){
+            start+=strlen(command);
+            char chatId[255];
+            getCommand(start,line,chatId);
+            int res=goTo(chatId);
+            strcpy(curChatId,chatId);
+            menu();
+            }else if(strcmp(command,"main")==0){
+                state=1;
+                menu();
+            }
+    }else if(state==3){
+        if (strcmp(command,"sendmsg")==0){
+            char msg[5000];
+            getMsg(line,msg);
+            sendMsg(curChatId,msg);
+            state=1;
+            menu();
+        }else if(strcmp(command,"main")==0){
+                state=1;
+                menu();
+            }
+    }
+    
+}
 int main()
 {
+    menu();
 
     return 0;
 }
